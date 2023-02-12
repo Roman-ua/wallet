cc9bb46cd4b2f0653546c0146158ca24079c5788
roman148 <roman@u-coders.team>
2023-02-12 18:08:06 +0200

    fixed cooming soon screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from "next/image";
+import moon from '../../public/001-moon.png';
+import white from '../../public/001-white-balance.png';
+import {useState} from "react";
 
 export default function Home() {
+  const [modeToggle, setModeToggle] = useState(false);
+
   return (
-      <div>
-          <h1 className="text-3xl font-bold underline">
-              coming soon...
-          </h1>
+    <div className={!modeToggle ? 'dark' : ''}>
+      <div className="bg-white dark:bg-slate-800 h-screen flex items-center justify-center">
+        <div className="flex flex-col items-center">
+          <span className="dark:text-white text-3xl font-bold  mb-8">coming soon...</span>
+          <Image src={!modeToggle ? white : moon} alt="moon" onClick={() => setModeToggle(!modeToggle)} />
+        </div>
       </div>
+    </div>
   )
 }
